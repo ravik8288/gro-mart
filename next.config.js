232710923da3c4c7898079d2f2044242681072ed
ravik8288 <rkevadiya2@gmail.com>
2023-02-11 19:03:44 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  fontLoaders: [
+    { loader: '@next/font/google', options: { subsets: ['Poppins'] } },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
